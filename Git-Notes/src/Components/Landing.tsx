c75fbaf44942529bcdbcb1e1b4309bc9dc089550
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-26 23:20:48 +0530

    Styled a little!!!

--- Git-Notes/src/Components/Landing.tsx
+++ Git-Notes/src/Components/Landing.tsx
@@ -7,13 +7,17 @@ interface DepartmentCardProps {
 
 const DepartmentCard: React.FC<DepartmentCardProps> = ({ department, onClick }) => {
   return (
-    <div className="bg-blue-700 duration-200 hover:shadow-purple-600 hover:scale-105 hover:bg-blue-600 p-4 rounded-lg shadow-md cursor-pointer" onClick={() => onClick(department)}>
-      <h2 className="text-xl font-semibold">{department}</h2>
+    <div className=" bg-blue-800 duration-200 hover:shadow-red-500
+    hover:scale-105 hover:bg-blue-600 p-4 hover:text-black
+     rounded-lg shadow-lg h-28 cursor-pointer" onClick={() => onClick(department)}>
+      <h2 className="text-2xl mt-5 text-center font-semibold ">{department}</h2>
     </div>
   );
 };
 
-const departments: string[] = ['Computer Science', 'Electrical Engineering', 'Mechanical Engineering', 'Civil Engineering', 'Chemical Engineering', 'Biomedical Engineering'];
+const departments: string[] = ['Computer Science', 'Electrical Engineering',
+ 'Mechanical Engineering', 'Civil Engineering', 'Chemical Engineering', 
+ 'Biomedical Engineering'];
 
 const Landing: React.FC = () => {
   const handleDepartmentClick = (department: string) => {
@@ -22,9 +26,9 @@ const Landing: React.FC = () => {
   };
 
   return (
-    <div className='mt-10'>
-      <h1 className="text-2xl  text-center font-bold mb-4 text-red-700 shadow-glow">Select your Department</h1>
-      <div className="grid m-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className=' mt-20'>
+      <h1 className="text-4xl  text-center font-bold mb-4 text-red-700 shadow-glow">Select your Department</h1>
+      <div className="grid  m-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {departments.map((department, index) => (
           <DepartmentCard key={index} department={department} onClick={handleDepartmentClick} />
         ))}
