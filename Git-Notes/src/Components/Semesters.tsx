c65bf0f07026eecc96fa9ff13b29b4d36ac14a6e
Omkar Patil <git21cs072-t@students.git.edu>
2024-02-27 20:13:24 +0530

    Added Sewmester Page!!!

---
+++ Git-Notes/src/Components/Semesters.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {useNavigate} from "react-router-dom";
+
+const SemesterCard: React.FC<{ semester: number; onClick: (semester: number) => void }> = ({ semester, onClick }) => {
+    return (
+      <div className="bg-green-800 duration-200 hover:shadow-red-500 hover:scale-105 hover:bg-green-600 p-4 hover:text-black rounded-lg shadow-lg h-28 cursor-pointer" onClick={() => onClick(semester)}>
+        <h2 className="text-2xl mt-5 text-center font-semibold">Semester {semester}</h2>
+      </div>
+    );
+  };
+
+const Semesters: React.FC = () => {
+    const navigate = useNavigate();
+  // Array to hold semesters from 1 to 8
+  // Function to handle click on semester
+  const handleSemesterClick = (semester: number) => {
+    // Handle semester click event
+    console.log(`Selected semester: ${semester}`);
+    // Navigate to '/Semester' with the selected semester as a parameter
+    navigate(`/Semester/${semester}`);
+  };
+
+  return (
+    <div className='mt-10'>
+      <h2>Semesters</h2>
+      {/* Map through semesters array to render clickable elements */}
+      <h1 className="text-4xl text-center font-bold mb-4 text-red-700 shadow-glow">Select Semester</h1>
+      <div className="grid m-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {/* Generating Semester Cards */}
+        {Array.from({ length: 8 }, (_, index) => index + 1).map((semester, index) => (
+          <SemesterCard key={index} semester={semester} onClick={handleSemesterClick} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Semesters;
